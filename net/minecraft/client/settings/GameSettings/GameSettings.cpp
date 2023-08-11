7e08a5a744144fc8c48cc79fa11b819086119b98
Lefraudeur <karlbouisseren@gmail.com>
2023-08-11 21:47:12 +0200

    idk

--- net/minecraft/client/settings/GameSettings/GameSettings.cpp
+++ net/minecraft/client/settings/GameSettings/GameSettings.cpp
@@ -4,9 +4,9 @@ float GameSettings::getGammaSetting()
 {
 	if(!instance) return 0.0f;
 	if (Ripterms::majorVersion == Ripterms::Version::MAJOR_1_16_5) {
-		return Ripterms::p_env->GetDoubleField(instance, Ripterms::classcache->GameSettingsClass.fields["gammaSetting"]);
+		return (float)Ripterms::p_env->GetDoubleField(instance, Ripterms::classcache->GameSettingsClass.fields["gammaSetting"]);
 	}
-	return (float)Ripterms::p_env->GetFloatField(instance, Ripterms::classcache->GameSettingsClass.fields["gammaSetting"]);
+	return Ripterms::p_env->GetFloatField(instance, Ripterms::classcache->GameSettingsClass.fields["gammaSetting"]);
 }
 
 void GameSettings::setGammaSetting(float value)
